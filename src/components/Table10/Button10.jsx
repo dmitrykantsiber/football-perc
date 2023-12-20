5ffcba9286960d838bf96bb041dc5088b88f8151
dmitrykantsiber <101928155+dmitrykantsiber@users.noreply.github.com>
2023-12-20 13:19:34 +0200

    Update Button10.jsx

--- src/components/Table10/Button10.jsx
+++ src/components/Table10/Button10.jsx
@@ -17,7 +17,7 @@ const Button13 = ({setValues}) => {
   }
   return (
     <div className="button">
-      <input style={{marginTop:"20px", width:"200px"}} type="number" value={inputValue} onChange={handleOnChange}/>
+      <input style={{marginTop:"20px", width:"200px"}} type="number" value={inputValue || 1} onChange={handleOnChange}/>
       <button style={{width:"200px", height:"100px", fontSize:"25px", margin:"20px"}} onClick={handleSubmit}>Генерировать</button>
     </div>
   );
